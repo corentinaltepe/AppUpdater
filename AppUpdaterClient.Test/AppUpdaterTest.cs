7c64588f87623c3f63135e9bee46b6c0c224ba13
Corentin Altepe <corentin.altepe@gmail.com>
2016-12-10 20:21:24 +0300

    Created Client application

---
+++ AppUpdaterClient.Test/AppUpdaterTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AppUpdaterClient.Test
+{
+    [TestClass]
+    public class AppUpdaterTest
+    {
+        private static string SERVER = "http://localhost:49473/";
+        [TestMethod]
+        public void CheckNewerVersionAvailableTest()
+        {
+            AppUpdater updater = new AppUpdater();
+            updater.ServerAddress = SERVER;
+
+            updater.CheckNewerVersionAvailable();
+        }
+    }
+}
---
+++ AppUpdaterClient/AppUpdater.cs
@@ -0,0 +1,37 @@
+﻿using AppUpdaterService.Models;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppUpdaterClient
+{
+    public class AppUpdater
+    {
+        public string ServerAddress { get; set; }
+        public string AppId { get; set; }
+        public int CurrentVersion { get; set; }
+        public App NewerApp { get; set; }
+
+        public AppUpdater()
+        {
+
+        }
+
+        // Check on the server if a newer version is available for the given app
+        // If newer app is available, assign it to this.NewerApp
+        public void CheckNewerVersionAvailable()
+        {
+            var client = new RestClient(ServerAddress);
+            // client.Authenticator = new HttpBasicAuthenticator(username, password);
+
+            var request = new RestRequest("Apps/", Method.GET);
+            
+            var asyncHandle = client.ExecuteAsync<App>(request, response => {
+                Console.WriteLine(response.Data.Name);
+            });
+        }
+    }
+}
