d34e0d11cb181b587b7b1de22a6899bebca3ce4d
Corentin Altepe <corentin.altepe@gmail.com>
2016-12-10 18:52:29 +0300

    Created model and controller

--- AppUpdaterService/Controllers/AppsController.cs
+++ AppUpdaterService/Controllers/AppsController.cs
@@ -1,25 +1,34 @@
-﻿using AppUpdaterService.Utils;
+﻿using AppUpdaterService.Models;
+using AppUpdaterService.Utils;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Web;
 using System.Web.Http;
+using System.Xml;
 
 namespace AppUpdaterService.Controllers
 {
     public class AppsController : ApiController
     {
+        #region HTTP
         // GET: api/Apps
-        public IEnumerable<string> Get()
+        public IEnumerable<App> Get()
         {
-            return new string[] { "value1", "value2" };
+            return ReadListOfApps();
         }
 
         // GET: api/Apps/5
-        public string Get(int id)
+        public App Get(int id)
         {
-            return "value";
+            List<App> apps = ReadListOfApps();
+            foreach (App app in apps)
+                if (app.Id.Equals(Convert.ToString(id)))
+                    return app;
+
+            return null;
         }
 
         // POST: api/Apps
@@ -40,5 +49,25 @@ namespace AppUpdaterService.Controllers
         public void Delete(int id)
         {
         }
+
+        #endregion
+
+        private List<App> ReadListOfApps()
+        {
+            List<App> apps = new List<App>();
+
+            XmlTextReader reader = new XmlTextReader(HttpRuntime.AppDomainAppPath+"/App_Data/AppsList.xml");
+            while (reader.Read())
+            {
+                if(reader.NodeType == XmlNodeType.Element && reader.Name == "App")
+                {
+                    apps.Add(App.ReadXML(reader));
+                }
+            }
+
+            return apps;
+        }
+
+
     }
 }
--- AppUpdaterService/Models/App.cs
+++ AppUpdaterService/Models/App.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Xml;
 
 namespace AppUpdaterService.Models
 {
@@ -12,5 +13,51 @@ namespace AppUpdaterService.Models
         public string VersionStr { get; set; }
         public int Version { get; set; }
         public bool Supported { get; set; }
+
+        public static App ReadXML(XmlTextReader reader)
+        {
+            App app = new App();
+
+            while (reader.Read() && 
+                    !(reader.NodeType == XmlNodeType.EndElement && reader.Name == "App"))
+            {
+                if(reader.NodeType == XmlNodeType.Element)
+                {
+                    switch(reader.Name)
+                    {
+                        case "Id":
+                            reader.Read();
+                            app.Id = reader.Value;
+                            break;
+
+                        case "Name":
+                            reader.Read();
+                            app.Name = reader.Value;
+                            break;
+
+                        case "VersionStr":
+                            reader.Read();
+                            app.VersionStr = reader.Value;
+                            break;
+
+                        case "Version":
+                            reader.Read();
+                            app.Version = XmlConvert.ToInt32(reader.Value);
+                            break;
+
+                        case "Supported":
+                            reader.Read();
+                            app.Supported = XmlConvert.ToBoolean(reader.Value);
+                            break;
+
+                        default:
+                            // Do something else, then break;
+                            break;
+                    }
+                }
+            }
+
+            return app;
+        }
     }
 }
